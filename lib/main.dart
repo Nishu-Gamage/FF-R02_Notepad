60c06337b7b8424b8f5a67136854ef7318e7492d
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-11-11 17:25:54 +0900

    [Body] - TabBarView (UI -1)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() => runApp(
-  DevicePreview(
-    builder: (context) => MyNoteApp(), // Wrap your app
-  ),
-);
-
+      DevicePreview(
+        builder: (context) => MyNoteApp(), // Wrap your app
+      ),
+    );
 
 class MyNoteApp extends StatelessWidget {
   const MyNoteApp({super.key});
@@ -24,7 +23,10 @@ class MyNoteApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title,});
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
 
   final String title;
 
@@ -38,30 +40,123 @@ class _MyHomePageState extends State<MyHomePage> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(          
+        appBar: AppBar(
           title: Text(widget.title),
           actions: [
             IconButton(
-              onPressed: (){}, 
+              onPressed: () {},
               icon: Icon(Icons.search),
             ),
             IconButton(
-              onPressed: (){}, 
+              onPressed: () {},
               icon: Icon(Icons.more_horiz),
             )
           ],
           flexibleSpace: Image.asset(
-            "assets/AppBar-Background.jpg", 
-            fit:BoxFit.cover,
+            "assets/AppBar-Background.jpg",
+            fit: BoxFit.cover,
           ),
           bottom: const TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.note), text: 'All Notes',),
-              Tab(icon: Icon(Icons.add), text: 'Add Notes',),
+              Tab(
+                icon: Icon(Icons.note),
+                text: 'All Notes',
+              ),
+              Tab(
+                icon: Icon(Icons.add),
+                text: 'Add Notes',
+              ),
             ],
           ),
-          ),        
+          elevation: 5.0,
+        ),
+        body: TabBarView(
+          children: [
+            displayAllNote(),
+            addNote(),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget addNote() {
+  TextEditingController addNoteTitle = TextEditingController();
+  TextEditingController addNewNote = TextEditingController();
+
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        // Container(
+        //   alignment: Alignment.center,
+        //   padding: EdgeInsets.all(10),
+        //   margin: const EdgeInsets.only(top: 10),
+        //   child: Text(
+        //     'Add New Note',
+        //     style: TextStyle(
+        //         color: Colors.red, fontWeight: FontWeight.w500, fontSize: 25),
+        //   ),
+        // ),
+        Container(
+          margin: EdgeInsets.only(top: 50),
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: addNoteTitle,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Note Title',
+            ),
+          ),
+        ),
+        Container(           
+          margin: EdgeInsets.only(top: 20),
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: addNewNote,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Add Note',
+            ),
+          ),
+        ),
+        Container(
+          height: 50,
+          // padding: EdgeInsets.all(10),
+          margin: EdgeInsets.only(top: 20),
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: ElevatedButton(
+              child: Text('Submit'), 
+              onPressed: () {},
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget displayAllNote() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        ListTile(
+          leading: Icon(Icons.brightness_auto),
+          title: Text('Note 1'),
+          subtitle: Text("sub note"),
+          trailing: Icon(Icons.menu),
+          onTap: () {},
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Note 2'),
+          subtitle: Text("sub note"),
+          trailing: Icon(Icons.menu),
+          onTap: () {},
+        ),
+      ],
+    ),
+  );
 }
